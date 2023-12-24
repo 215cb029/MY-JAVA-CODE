c7a40b406ceccb4285724e517abc0f066c2c3aa5
suryaranjan-Spring <suryaranjan83@gmail.com>
2023-12-25 00:19:18 +0530

    commit

---
+++ src/BracketMatching.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class BracketMatching {
+
+        public static void main(String[] args) {
+            String bracket = "[({})]";
+            System.out.println(validation(bracket));
+        }
+
+        public static boolean validation(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') {
+                    stack.push(')');
+                } else if (c == '{') {
+                    stack.push('}');
+                } else if (c == '[') {
+                    stack.push(']');
+                } else if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+
+            }
+            return stack.isEmpty();
+
+        }
+    }
+
+
