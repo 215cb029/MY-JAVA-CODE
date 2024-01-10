270294a9ca4e59059ff381d9eebad0f1ad7b066f
suryaranjan-Spring <suryaranjan83@gmail.com>
2024-01-10 10:35:20 +0530

    commit

---
+++ src/StarPattern_1.java
@@ -0,0 +1,13 @@
+public class StarPattern_1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<2*i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
